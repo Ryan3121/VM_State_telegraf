07ef416165a73f10d6a5b1f0625e2f12ad2c4d47
Ryan3121 <50601318+Ryan3121@users.noreply.github.com>
2021-01-10 21:16:06 +0000

    Update vmstate.ps1

--- vmstate.ps1
+++ vmstate.ps1
@@ -17,8 +17,6 @@ Foreach ($VM in $VMs) {
     $Stateuse = "VM_state=" + $state3
     $VM_state = "VM_State"
     $postp =  "$VM_state,$Name $Stateuse"
-    $post = Write-Output $postp
-
 
 Invoke-WebRequest 'http://192.168.0.58:8086/write?db=dbname&u=user&p=pass' -method post  -body "$postp"
 }
